407227f33dfe9637fa2e82572c6578d8baaff889
HARISH RAYALA <100848582+HarishRayala@users.noreply.github.com>
2022-05-02 12:25:49 +0530

    news app

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,11 @@
 // Ude Import export (MANDATORY)
 // Onclicking store the news in local storage with key "news" so that you can access that on news.html page
 
-let url=`https://masai-mock-api.herokuapp.com/news/top-headlines?country=in`
-    fetch (url)
-    .then(function(res){
-      return  res.json()
-    })
-    .then(function(res){
-        // console.log(res)
-        appendData(res)
-    })
-    .catch(function(err){
-        console.log("err:",err)
-    })
 
-function appendData(data){
-    console.log(data)
-    let container= document.getElementById("results");
-    let p1=document.createElement("p")
-    p1.innerHTML=data.author
-    container.append(p1)
-}
+import  searchbyId  from "./news";
+let searchbyid=searchbyId
+
+
 
 
 
